640364660de562f7b4bd9329d08ec1174ea1fa96
geethap <geethasrilakshmi96@gmail.com>
2022-07-05 10:47:29 +0530

    refactor

---
+++ StockMarketRegistration/src/main/java/com/stockmarket/constants/RegistrationContants.java
@@ -0,0 +1,8 @@
+package com.stockmarket.constants;
+
+public class RegistrationContants {
+
+	public static final String ALREADYEXISTS="Company already exists";
+	public static final String TURNOVER_ERROR="Company turnover should be greater than 10crs";
+	public static final String COMPANYDOESNOTEXIST="No Company exists with given Id";
+}
---
+++ StockPricing/src/main/java/com/stockmarket/exception/StockPricingExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.stockmarket.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.stockmarket.helper.ErrorInfo;
+
+
+@RestControllerAdvice
+public class StockPricingExceptionHandler {
+	
+	private static final Logger LOGGER= LoggerFactory.getLogger(StockPricingExceptionHandler.class);
+	
+	
+	@ExceptionHandler(StockMarketException.class)
+	public ResponseEntity<ErrorInfo> stockMarketExceptionHandler(StockMarketException exception){
+		LOGGER.error(exception.getMessage(),exception);
+		ErrorInfo errorInfo=new ErrorInfo();
+		errorInfo.setErrorCode(HttpStatus.BAD_REQUEST.value());
+		errorInfo.setErrorMessage((exception.getMessage()));
+		return new ResponseEntity<>(errorInfo,HttpStatus.BAD_REQUEST);
+		}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorInfo> generalExceptionHandler(Exception exception){
+		LOGGER.error(exception.getMessage(),exception);
+		ErrorInfo errorInfo=new ErrorInfo();
+		errorInfo.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		errorInfo.setErrorMessage("Something went wrong");
+		return new ResponseEntity<>(errorInfo,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+
+	
+}
